6ea4d83ec0de5116033752081bb11f2ec06cf5d5
Christopher Aue <mail@christopheraue.net>
2015-12-17 23:49:14 +0100

    added sys/ and dev/ paths

--- lib/uinput/device.rb
+++ lib/uinput/device.rb
@@ -6,6 +6,8 @@ require_relative "device/error"
 
 module Uinput
   class Device
+    SYS_INPUT_DIR = '/sys/devices/virtual/input/'
+
     def initialize(&block)
       @file = self.class::SystemInitializer.new(self, &block).create
     end
@@ -13,6 +15,25 @@ module Uinput
     def destroy
       @file.ioctl(UI_DEV_DESTROY, nil)
       @file = nil
+    end
+
+    def sysname
+      @sysname ||= begin
+        name_len = 128
+        name_buffer = [' ' * name_len].pack("A#{name_len}")
+        @file.ioctl(Uinput.UI_GET_SYSNAME(name_len), name_buffer)
+        name_buffer.unpack("A#{name_len}").first
+      end
+    end
+
+    def sys_path
+      "#{SYS_INPUT_DIR}#{sysname}"
+    end
+
+    def dev_path
+      event_dir = Dir["#{sys_path}/event*"].first
+      event = File.basename(event_dir)
+      "/dev/input/#{event}"
     end
 
     def active?
